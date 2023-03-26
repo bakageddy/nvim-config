0dc0caa40b97e1307bfa29bb45a9607da1e5768a
Dinesh Kumar G <gdineshda@gmail.com>
2023-03-26 11:23:36 +0530

    Added GitIgnore

---
+++ lua/dinesh/themes.lua
@@ -0,0 +1,43 @@
+local M = {}
+M.gruvbox_flat = {
+  bg       = '#1d2021',
+  fg       = '#d4be98',
+  yellow   = '#d8a657',
+  cyan     = '#89b482',
+  darkblue = '#7daea3',
+  green    = '#a9b665',
+  orange   = '#e78a4e',
+  violet   = '#d3869b',
+  magenta  = '#d3869b',
+  blue     = '#7daea3',
+  red      = '#ea6962',
+}
+
+M.gruvbox = {
+  bg       = '#cc1d2021',
+  fg       = '#ebdbb2',
+  yellow   = '#fabd2f',
+  cyan     = '#8ec07c',
+  darkblue = '#83a598',
+  green    = '#b8bb26',
+  orange   = '#fe8019',
+  violet   = '#d3869b',
+  magenta  = '#d3869b',
+  blue     = '#83a598',
+  red      = '#fb4934',
+}
+
+M.nord = {
+  bg       = '#cc2E3440',
+  fg       = '#D8DEE9',
+  yellow   = '#EBCB8B',
+  cyan     = '#8FBCBB',
+  darkblue = '#81A1C1',
+  green    = '#A3B38C',
+  orange   = '#D08770',
+  violet   = '#B48EAD',
+  magenta  = '#B48EAD',
+  blue     = '#88C0D0',
+  red      = '#BF616A',
+}
+return M
