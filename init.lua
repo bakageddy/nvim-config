c9d8f9e85f4ec763e1a84ef685670154af213674
Dinesh Kumar <bakageddy@gmail.com>
2023-06-29 20:39:03 +0530

    Hehe

--- init.lua
+++ init.lua
@@ -1,2 +1,9 @@
 require("dinesh")
 require("impatient")
+
+vim.g.neovide_padding_right = 10
+vim.g.neovide_padding_left  = 10
+vim.g.neovide_padding_top   = 10
+vim.g.neovide_padding_down  = 10
+
+vim.o.guifont="Iosevka"
