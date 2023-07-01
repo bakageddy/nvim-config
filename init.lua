f09f81fd516e4144cd2334be8e7df1769f24659b
Dinesh Kumar <bakageddy@gmail.com>
2023-07-01 10:34:33 +0530

    ADD: neovide config

--- init.lua
+++ init.lua
@@ -1,2 +1,15 @@
 require("impatient")
 require("dinesh")
+
+vim.g.neovide_padding_left = 10
+vim.g.neovide_padding_right = 10
+vim.g.neovide_padding_top = 10
+vim.g.neovide_padding_bottom = 10
+
+vim.g.neovide_cursor_vfx_mode="pixiedust"
+vim.g.neovide_cursor_vfx_particle_density=14.0
+
+vim.g.neovide_refresh_rate = 60
+vim.g.neovide_refresh_rate_idle = 5
+
+vim.o.guifont="Iosevka"
